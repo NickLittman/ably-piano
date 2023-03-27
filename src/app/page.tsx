e17eae4d2e6aa7e499555e97393e8f666133b6a6
Nick Littman <nlittman@Nicks-Laptop.lan>
2023-03-27 11:25:41 -0400

    fixed next/js image rendering issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import Temp from "./components/Temp";
 import Hero from "./components/Hero";
 import NavBar from "./components/NavBar";
+import '../../globals.css'
 
 const inter = Inter({ subsets: ["latin"] });
 
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -5,21 +5,22 @@ import { Container, Row, Col } from "reactstrap";
 
 import Loading from "../components/Loading";
 import { useAuth0, withAuthenticationRequired } from "@auth0/auth0-react";
-// import Image from "next/image";
+import Image from "next/image";
 
-export const ProfileComponent = () => {
+export function Profile() {
   const { user } = useAuth0();
   if (!user) throw new Error("no user");
   return (
     <Container className="mb-5">
       <Row className="align-items-center profile-header mb-5 text-center text-md-left">
         <Col md={2}>
-          <img
+          {/* <img
             src={user.picture || "/profile_image.png"}
             alt="Profile"
             className="rounded-circle img-fluid profile-picture mb-3 mb-md-0"
             referrerPolicy="no-referrer"
-          />
+          /> */}
+          <Image src={user.picture || "/profile_image.png"} alt="Profile" width={200} height={200} className="rounded-circle img-fluid profile-picture mb-3 mb-md-0" referrerPolicy="no-referrer" />
         </Col>
         <Col md>
           <h2>{user.name}</h2>
@@ -30,6 +31,6 @@ export const ProfileComponent = () => {
   );
 };
 
-export default withAuthenticationRequired(ProfileComponent, {
+export default withAuthenticationRequired(Profile, {
   onRedirecting: () => <Loading />,
 });
